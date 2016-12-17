cd287667ea1fd1006b2abe33b071087be0ae7a36
utauchert <ulrich.tauchert@epost-dev.de>
2016-12-17 11:45:18 +0100

    Use Apache Commns now.

--- src/main/java/de/epost/kata/song/Song.java
+++ src/main/java/de/epost/kata/song/Song.java
@@ -1,10 +1,21 @@
 package de.epost.kata.song;
 
+import org.apache.commons.lang3.text.StrSubstitutor;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Song {
 
-    private String song =
+    private static final String lastStropheTemplate = "There was an old lady who swallowed a ${animal}...\n" +
+            "...She's dead, of course!";
+
+    private static final String firstStropheTemplate = "There was an old lady who swallowed a ${animal}.\n" +
+            "I don't know why she swallowed a ${animal} - perhaps she'll die!\n";
+
+
+    private String songTemplate =
             "\n" +
                     "There was an old lady who swallowed a @1@;\n" +
                     "That wriggled and wiggled and tickled inside her.\n" +
@@ -41,76 +52,54 @@ class Song {
                     "She swallowed the @1@ to catch the @0@;\n" +
                     "I don't know why she swallowed a @0@ - perhaps she'll die!\n" +
                     "\n";
+
     private Theme theme;
 
     public Song() {
         this.theme = new FarmTheme();
-        //this.song = buildSong(theme.getAnimals());
     }
 
     public Song(Theme theme) {
         this.theme = theme;
     }
 
-    //    private String buildSong(List<String> animals) {
-//        String song = "";
-//        for (int i = 0; i < animals.size() - 1; i++) {
-//            song += buildStrophe(animals.subList(0, i));
-//        }
-//        song += buildLastStrophe(animals.get(animals.size()-1));
-//
-//        return song;
     public String playSong() {
         return playSong(theme.getAnimals());
     }
 
     private String playSong(List<String> animals) {
-        String firstStrophe = addFirstStrophe(animals.get(0));
-
-        String middle = song;
-        for (int i = 0; i < animals.size() - 1; i++) {
-            middle = middle.replaceAll("@" + i + "@", animals.get(i));
-        }
-
-        firstStrophe += middle;
-        firstStrophe += addLastStrophe(animals.get(animals.size() - 1));
-        return firstStrophe;
+        String song = addFirstStrophe(animals.get(0));
+        song += addMiddleStrophes(animals);
+        song += addLastStrophe(animals.get(animals.size() - 1));
+        return song;
     }
 
     private String addFirstStrophe(String animal) {
-        return "There was an old lady who swallowed a " + animal + ".\n" +
-                "I don't know why she swallowed a " + animal + " - perhaps she'll die!\n";
+        Map<String, String> values = new HashMap<>();
+        values.put("animal", animal);
+
+        StrSubstitutor sub = new StrSubstitutor(values);
+        return sub.replace(firstStropheTemplate);
+    }
+
+    private String addMiddleStrophes(List<String> animals) {
+        String middle = songTemplate;
+        for (int i = 0; i < animals.size() - 1; i++) {
+            middle = middle.replaceAll("@" + i + "@", animals.get(i));
+        }
+        return middle;
     }
 
     private String addLastStrophe(String animal) {
-        return "There was an old lady who swallowed a " + animal + "...\n" +
-                "...She's dead, of course!";
+        Map<String, String> values = new HashMap<>();
+        values.put("animal", animal);
+
+        StrSubstitutor sub = new StrSubstitutor(values);
+        return sub.replace(lastStropheTemplate);
     }
-
-
-//    }
 
     public static void main(String[] args) {
         Song song = new Song();
         System.out.println(song.playSong());
     }
-/*
-    private String buildStrophe(List<String> animals, String secondLine) {
-        String strophe = "There was an old lady who swallowed a " + animals.get(0) + ".\n";
-
-
-        strophe += secondLine.replace("@animal@", animals.get(1));
-
-
-
-
-
-        strophe +=
-    }
-
-    private String buildLastStrophe(String animal) {
-        return "There was an old lady who swallowed a " + animal + "...\n" +
-                "...She's dead, of course!";
-    }
-    */
 }
