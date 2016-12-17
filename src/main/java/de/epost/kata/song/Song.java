b45e332119d81745cfae1a3bf5eaf09e2c4db94b
utauchert <ulrich.tauchert@epost-dev.de>
2016-12-17 19:33:39 +0100

    Extract all methods for the strophes

--- src/main/java/de/epost/kata/song/Song.java
+++ src/main/java/de/epost/kata/song/Song.java
@@ -17,7 +17,7 @@ class Song {
                     "I don't know why she swallowed a ${animal} - perhaps she'll die!\n";
 
 
-    private String songTemplate =
+//    private String songTemplate =
 //            "\n" +
 //                    "There was an old lady who swallowed a @1@;\n" +
 //                    "That wriggled and wiggled and tickled inside her.\n" +
@@ -29,31 +29,31 @@ class Song {
 //                    "She swallowed the @2@ to catch the @1@,\n" +
 //                    "She swallowed the @1@ to catch the @0@;\n" +
 //                    "I don't know why she swallowed a @0@ - perhaps she'll die!\n" +
-            "\n" +
-                    "There was an old lady who swallowed a @3@;\n" +
-                    "Fancy that to swallow a @3@!\n" +
-                    "She swallowed the @3@ to catch the @2@,\n" +
-                    "She swallowed the @2@ to catch the @1@,\n" +
-                    "She swallowed the @1@ to catch the @0@;\n" +
-                    "I don't know why she swallowed a @0@ - perhaps she'll die!\n" +
-                    "\n" +
-                    "There was an old lady who swallowed a @4@;\n" +
-                    "What a hog, to swallow a @4@!\n" +
-                    "She swallowed the @4@ to catch the @3@,\n" +
-                    "She swallowed the @3@ to catch the @2@,\n" +
-                    "She swallowed the @2@ to catch the @1@,\n" +
-                    "She swallowed the @1@ to catch the @0@;\n" +
-                    "I don't know why she swallowed a @0@ - perhaps she'll die!\n" +
-                    "\n" +
-                    "There was an old lady who swallowed a @5@;\n" +
-                    "I don't know how she swallowed a @5@!\n" +
-                    "She swallowed the @5@ to catch the @4@,\n" +
-                    "She swallowed the @4@ to catch the @3@,\n" +
-                    "She swallowed the @3@ to catch the @2@,\n" +
-                    "She swallowed the @2@ to catch the @1@,\n" +
-                    "She swallowed the @1@ to catch the @0@;\n" +
-                    "I don't know why she swallowed a @0@ - perhaps she'll die!\n" +
-                    "\n";
+//            "\n" +
+//                    "There was an old lady who swallowed a @3@;\n" +
+//                    "Fancy that to swallow a @3@!\n" +
+//                    "She swallowed the @3@ to catch the @2@,\n" +
+//                    "She swallowed the @2@ to catch the @1@,\n" +
+//                    "She swallowed the @1@ to catch the @0@;\n" +
+//                    "I don't know why she swallowed a @0@ - perhaps she'll die!\n" +
+//                    "\n" +
+//                    "There was an old lady who swallowed a @4@;\n" +
+//                    "What a hog, to swallow a @4@!\n" +
+//                    "She swallowed the @4@ to catch the @3@,\n" +
+//                    "She swallowed the @3@ to catch the @2@,\n" +
+//                    "She swallowed the @2@ to catch the @1@,\n" +
+//                    "She swallowed the @1@ to catch the @0@;\n" +
+//                    "I don't know why she swallowed a @0@ - perhaps she'll die!\n" +
+//            "\n" +
+//                    "There was an old lady who swallowed a @5@;\n" +
+//                    "I don't know how she swallowed a @5@!\n" +
+//                    "She swallowed the @5@ to catch the @4@,\n" +
+//                    "She swallowed the @4@ to catch the @3@,\n" +
+//                    "She swallowed the @3@ to catch the @2@,\n" +
+//                    "She swallowed the @2@ to catch the @1@,\n" +
+//                    "She swallowed the @1@ to catch the @0@;\n" +
+//                    "I don't know why she swallowed a @0@ - perhaps she'll die!\n" +
+//                    "\n";
 
     private Theme theme;
 
@@ -85,18 +85,17 @@ class Song {
     }
 
     private String addMiddleStrophes(List<String> animals) {
+        String middleSong = addSecondStrophe(animals);
+        middleSong += addThirdStrophe(animals);
+        middleSong += addFourthStrophe(animals);
+        middleSong += addFifthStrophe(animals);
+        middleSong += addSixthStrophe(animals);
+        middleSong += "\n";
 
-        String second = addSecondStrophe(animals.get(0), animals.get(1));
-        second += addThirdStrophe(animals.get(0), animals.get(1), animals.get(2));
-
-        String middle = songTemplate;
-        for (int i = 0; i < animals.size() - 1; i++) {
-            middle = middle.replaceAll("@" + i + "@", animals.get(i));
-        }
-        return second + middle;
+        return middleSong;
     }
 
-    private String addSecondStrophe(String animal0, String animal1) {
+    private String addSecondStrophe(List<String> animals) {
         String strophe = "\n" +
                 "There was an old lady who swallowed a ${animal1};\n" +
                 "That wriggled and wiggled and tickled inside her.\n" +
@@ -104,14 +103,14 @@ class Song {
                 "I don't know why she swallowed a ${animal0} - perhaps she'll die!\n";
 
         Map<String, String> values = new HashMap<>();
-        values.put("animal0", animal0);
-        values.put("animal1", animal1);
+        values.put("animal0", animals.get(0));
+        values.put("animal1", animals.get(1));
 
         StrSubstitutor sub = new StrSubstitutor(values);
         return sub.replace(strophe);
     }
 
-    private String addThirdStrophe(String animal0, String animal1, String animal2) {
+    private String addThirdStrophe(List<String> animals) {
         String strophe = "\n" +
                 "There was an old lady who swallowed a ${animal2};\n" +
                 "How absurd to swallow a ${animal2}.\n" +
@@ -120,9 +119,72 @@ class Song {
                 "I don't know why she swallowed a ${animal0} - perhaps she'll die!\n";
 
         Map<String, String> values = new HashMap<>();
-        values.put("animal0", animal0);
-        values.put("animal1", animal1);
-        values.put("animal2", animal2);
+        values.put("animal0", animals.get(0));
+        values.put("animal1", animals.get(1));
+        values.put("animal2", animals.get(2));
+
+        StrSubstitutor sub = new StrSubstitutor(values);
+        return sub.replace(strophe);
+    }
+
+    private String addFourthStrophe(List<String> animals) {
+        String strophe = "\n" +
+                "There was an old lady who swallowed a ${animal3};\n" +
+                "Fancy that to swallow a ${animal3}!\n" +
+                "She swallowed the ${animal3} to catch the ${animal2},\n" +
+                "She swallowed the ${animal2} to catch the ${animal1},\n" +
+                "She swallowed the ${animal1} to catch the ${animal0};\n" +
+                "I don't know why she swallowed a ${animal0} - perhaps she'll die!\n";
+
+        Map<String, String> values = new HashMap<>();
+        values.put("animal0", animals.get(0));
+        values.put("animal1", animals.get(1));
+        values.put("animal2", animals.get(2));
+        values.put("animal3", animals.get(3));
+
+        StrSubstitutor sub = new StrSubstitutor(values);
+        return sub.replace(strophe);
+    }
+
+    private String addFifthStrophe(List<String> animals) {
+        String strophe = "\n" +
+                "There was an old lady who swallowed a ${animal4};\n" +
+                "What a hog, to swallow a ${animal4}!\n" +
+                "She swallowed the ${animal4} to catch the ${animal3},\n" +
+                "She swallowed the ${animal3} to catch the ${animal2},\n" +
+                "She swallowed the ${animal2} to catch the ${animal1},\n" +
+                "She swallowed the ${animal1} to catch the ${animal0};\n" +
+                "I don't know why she swallowed a ${animal0} - perhaps she'll die!\n";
+
+        Map<String, String> values = new HashMap<>();
+        values.put("animal0", animals.get(0));
+        values.put("animal1", animals.get(1));
+        values.put("animal2", animals.get(2));
+        values.put("animal3", animals.get(3));
+        values.put("animal4", animals.get(4));
+
+        StrSubstitutor sub = new StrSubstitutor(values);
+        return sub.replace(strophe);
+    }
+
+    private String addSixthStrophe(List<String> animals) {
+        String strophe = "\n" +
+                "There was an old lady who swallowed a ${animal5};\n" +
+                "I don't know how she swallowed a ${animal5}!\n" +
+                "She swallowed the ${animal5} to catch the ${animal4},\n" +
+                "She swallowed the ${animal4} to catch the ${animal3},\n" +
+                "She swallowed the ${animal3} to catch the ${animal2},\n" +
+                "She swallowed the ${animal2} to catch the ${animal1},\n" +
+                "She swallowed the ${animal1} to catch the ${animal0};\n" +
+                "I don't know why she swallowed a ${animal0} - perhaps she'll die!\n";
+
+        Map<String, String> values = new HashMap<>();
+        values.put("animal0", animals.get(0));
+        values.put("animal1", animals.get(1));
+        values.put("animal2", animals.get(2));
+        values.put("animal3", animals.get(3));
+        values.put("animal4", animals.get(4));
+        values.put("animal5", animals.get(5));
 
         StrSubstitutor sub = new StrSubstitutor(values);
         return sub.replace(strophe);
