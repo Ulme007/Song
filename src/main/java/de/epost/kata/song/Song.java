5fc94ebbd34d7031d37fba54803e27c6c51f10d0
utauchert <ulrich.tauchert@epost-dev.de>
2016-12-18 08:39:37 +0100

    Make it a little bit simplify

--- src/main/java/de/epost/kata/song/Song.java
+++ src/main/java/de/epost/kata/song/Song.java
@@ -12,20 +12,15 @@ class Song {
     private static final String FIRST_STROPHE =
             "There was an old lady who swallowed a ${animal}.\n";
 
-    private static final String SECOND_STROPHE = "\n" +
-            "There was an old lady who swallowed a ${animal1};\n" +
-            "${animalRhyme1}\n" +
-            "She swallowed the ${animal1} to catch the ${animal};\n";
-
     private static final String INNER_STROPHE_START = "\n" +
             "There was an old lady who swallowed a ${animal};\n" +
             "${animalRhyme}\n";
 
     private static final String INNER_STROPHE_MIDDLE_WITH_COMMA =
-            "She swallowed the ${animal} to catch the ${animal1},\n";
+            "She swallowed the ${animal1} to catch the ${animal},\n";
 
     private static final String INNER_STROPHE_MIDDLE_WITH_SEMICOLON =
-            "She swallowed the ${animal} to catch the ${animal1};\n";
+            "She swallowed the ${animal1} to catch the ${animal};\n";
 
     private static final String LAST_STROPHE =
             "There was an old lady who swallowed a ${animal}...\n" +
@@ -50,7 +45,7 @@ class Song {
 
     private String playSong(List<Animal> animals) {
         String song = addFirstStrophe(animals.get(0));
-        song += addMiddleStrophes(animals);
+        song += addInnerStrophes(animals);
         song += addLastStrophe(animals.get(animals.size() - 1));
         return song;
     }
@@ -59,33 +54,20 @@ class Song {
         Map<String, String> values = new HashMap<>();
         values.put("animal", animal.getName());
 
-        StrSubstitutor sub = new StrSubstitutor(values);
-        return sub.replace(FIRST_STROPHE) + sub.replace(STROPHE_END);
+        return replace(FIRST_STROPHE, values) + replace(STROPHE_END, values);
     }
 
-    private String addMiddleStrophes(List<Animal> animals) {
-        String middleSong = addSecondStrophe(animals);
-
-        for (int i = 3; i < animals.size(); i++) {
-            middleSong += addThirdStrophe(animals, i);
+    private String addInnerStrophes(List<Animal> animals) {
+        String middleSong = "";
+        for (int i = 2; i < animals.size(); i++) {
+            middleSong += addInnerStrophe(animals, i);
         }
-
         middleSong += "\n";
 
         return middleSong;
     }
 
-    private String addSecondStrophe(List<Animal> animals) {
-        Map<String, String> values = new HashMap<>();
-        values.put("animal", animals.get(0).getName());
-        values.put("animal1", animals.get(1).getName());
-        values.put("animalRhyme1", animals.get(1).getRhyme());
-
-        StrSubstitutor sub = new StrSubstitutor(values);
-        return sub.replace(SECOND_STROPHE) + sub.replace(STROPHE_END);
-    }
-
-    private String addThirdStrophe(List<Animal> animals, int number) {
+    private String addInnerStrophe(List<Animal> animals, int number) {
         String start = addInnerStropheStart(animals.get(number - 1));
         String middle = addInnerStropheMiddle(animals, number);
         String end = addInnerStropheEnd(animals.get(0));
@@ -98,28 +80,25 @@ class Song {
         values.put("animal", animal.getName());
         values.put("animalRhyme", animal.getRhyme());
 
-        StrSubstitutor sub = new StrSubstitutor(values);
-        return sub.replace(INNER_STROPHE_START);
+        return replace(INNER_STROPHE_START, values);
     }
 
     private String addInnerStropheMiddle(List<Animal> animals, int number) {
         String result = "";
         for (int i = number - 1; i > 1; i--) {
             Map<String, String> values = new HashMap<>();
-            values.put("animal", animals.get(i).getName());
-            values.put("animal1", animals.get(i - 1).getName());
+            values.put("animal1", animals.get(i).getName());
+            values.put("animal", animals.get(i - 1).getName());
 
-            StrSubstitutor sub = new StrSubstitutor(values);
-            result += sub.replace(INNER_STROPHE_MIDDLE_WITH_COMMA);
+            result += replace(INNER_STROPHE_MIDDLE_WITH_COMMA, values);
         }
 
         if (number > 1) {
             Map<String, String> values = new HashMap<>();
-            values.put("animal", animals.get(1).getName());
-            values.put("animal1", animals.get(0).getName());
+            values.put("animal1", animals.get(1).getName());
+            values.put("animal", animals.get(0).getName());
 
-            StrSubstitutor sub = new StrSubstitutor(values);
-            result += sub.replace(INNER_STROPHE_MIDDLE_WITH_SEMICOLON);
+            result += replace(INNER_STROPHE_MIDDLE_WITH_SEMICOLON, values);
         }
 
         return result;
@@ -129,16 +108,19 @@ class Song {
         Map<String, String> values = new HashMap<>();
         values.put("animal", animal.getName());
 
-        StrSubstitutor sub = new StrSubstitutor(values);
-        return sub.replace(STROPHE_END);
+        return replace(STROPHE_END, values);
     }
 
     private String addLastStrophe(Animal animal) {
         Map<String, String> values = new HashMap<>();
         values.put("animal", animal.getName());
 
+        return replace(LAST_STROPHE, values);
+    }
+
+    private String replace(String template, Map<String, String> values) {
         StrSubstitutor sub = new StrSubstitutor(values);
-        return sub.replace(LAST_STROPHE);
+        return sub.replace(template);
     }
 
     public static void main(String[] args) {
